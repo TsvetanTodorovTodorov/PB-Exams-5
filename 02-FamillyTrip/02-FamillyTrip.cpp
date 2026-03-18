161da095c06b799d9b9d86bbe7a45461c4daa980
Tsvetan Todorov <111016955+TsvetanTodorovTodorov@users.noreply.github.com>
2026-03-18 09:28:53 +0200

    comment

--- 02-FamillyTrip/02-FamillyTrip.cpp
+++ 02-FamillyTrip/02-FamillyTrip.cpp
@@ -2,9 +2,39 @@
 
 int main()
 {
-	
+	double budget;
+	std::cin >> budget;
+	int nights;
+	std::cin >> nights;
+	double nightPrice;
+	std::cin >> nightPrice;
+	double percentExpenses;
+	std::cin >> percentExpenses;
 
+	if (nights > 7)
+	{
+		nightPrice *= 0.95;
+	}
 
+	double totalNightsPrice = nights * nightPrice;
+	double expenses = (percentExpenses / 100) * budget;
+	double totalExpenses = totalNightsPrice + expenses;
+
+	std::cout.setf(std::ios::fixed);
+	std::cout.precision(2);
+
+	if (budget >= totalExpenses)
+	{
+		std::cout << "Ivanovi will be left with "
+			<< budget - totalExpenses
+			<< " leva after vacation.";
+	}
+	else
+	{
+		std::cout << totalExpenses - budget
+			<< " leva needed.";
+	}
 
 	return 0;
 }
+	
